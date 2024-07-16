fa726b65f6aee0d8020aa33b74f1d6a284fa4349
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2024-07-17 01:36:18 +0200

    INFRA: Init Test Project

---
+++ WeAreDevelopers.Core.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. 
+//  W/ love for WeAreDevelopers World Congress 2024.
+// ---------------------------------------------------------------
+
+namespace WeAreDevelopers.Core.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
