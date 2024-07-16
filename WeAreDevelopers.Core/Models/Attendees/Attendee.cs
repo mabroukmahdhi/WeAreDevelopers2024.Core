f1924913be1dc4afa3c06fc77b694ddc96302b34
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2024-07-17 01:41:43 +0200

    DATA: Attendee

---
+++ WeAreDevelopers.Core/Models/Attendees/Attendee.cs
@@ -0,0 +1,16 @@
+// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. 
+//  W/ love for WeAreDevelopers World Congress 2024.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace WeAreDevelopers.Core.Models.Attendees
+{
+    public class Attendee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
